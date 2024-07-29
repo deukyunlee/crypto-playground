07adb77a961bab5c589074e64b1d3187df09fb1e
deukyunlee <dukedev1004@gmail.com>
2024-07-29 22:23:17 +0900

    FIX: tick calculation logic

--- axs-restake-reward/util/timeUtil.go
+++ axs-restake-reward/util/timeUtil.go
@@ -12,15 +12,6 @@ func IncrementTime(prevTime time.Time, duration time.Duration) time.Time {
 }
 
 func CalculateNextTick(now time.Time, lastTick time.Time) (time.Time, time.Duration) {
-	if lastTick.Add(24 * time.Hour).Before(now) {
-		return now, 0
-	}
-
-	nextTick := lastTick.Add(1 * time.Minute)
-
-	for nextTick.Before(now) {
-		nextTick = nextTick.Add(24 * time.Hour)
-	}
-
+	nextTick := lastTick.Add(24 * time.Hour)
 	return nextTick, nextTick.Sub(now)
 }
