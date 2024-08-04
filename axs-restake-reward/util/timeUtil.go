9302a04ac7f1d1aea5e51fb89ac13d47adf2fea8
deukyunlee <dukedev1004@gmail.com>
2024-08-04 20:48:11 +0900

    FEAT: wait for 24 hours + 1 second after restaking

--- axs-restake-reward/util/timeUtil.go
+++ axs-restake-reward/util/timeUtil.go
@@ -12,6 +12,6 @@ func IncrementTime(prevTime time.Time, duration time.Duration) time.Time {
 }
 
 func CalculateNextTick(now time.Time, lastTick time.Time) (time.Time, time.Duration) {
-	nextTick := lastTick.Add(24 * time.Hour).Add(10 * time.Second)
+	nextTick := lastTick.Add(24 * time.Hour).Add(1 * time.Second)
 	return nextTick, nextTick.Sub(now)
 }
